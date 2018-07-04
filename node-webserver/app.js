b50175bd286a76c53bfcd5bac7b958051df3c983
Ankuj Gupta <ankujgup@adobe.com>
2018-07-04 12:14:10 +0530

    More nodejs tutorials with hbs

--- node-webserver/app.js
+++ node-webserver/app.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
 const app = express()
+
+app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
   res.send("Hello World");
+})
+
+app.get("/help", (req, res)=> {
+  res.render("about.hbs", {
+    pageTitle : "About Page",
+    year: new Date().getFullYear()
+  })
+})
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/bad', (req, res)=> {
+  res.json({
+    errorMessage:"Unable to handle request"
+  });
 })
 
 app.listen("3000");
