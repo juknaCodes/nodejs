12f33d73180a92273df932e0057a4566cc540379
Ankuj Gupta <ankujgup@adobe.com>
2018-07-06 15:43:02 +0530

    Middleware nodejs

--- node-webserver/app.js
+++ node-webserver/app.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
+const fs = require('fs');
 const app = express()
 
 hbs.registerPartials(__dirname + '/views/partials');
 hbs.registerHelper('getYear', ()=> {
   return new Date().getFullYear();
 });
+
+app.use((req, res, next) => {
+  let date = new Date().toString();
+  let log = `${date}: request path is ${req.path}`;
+
+  console.log(log);
+  fs.appendFile('log.txt', log + '\n', (err) => {
+    if (err) throw err;
+    console.log('The "data to append" was appended to file!');
+  });
+  next();
+})
+
+app.use((req, res, next) => {
+  res.render("maintenance.hbs");
+})
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
