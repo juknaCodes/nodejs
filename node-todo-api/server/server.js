fe46254eae6455ed37c767ccc65a7534ac1da4ae
Ankuj Gupta <ankujgup@adobe.com>
2018-07-29 23:36:02 +0530

    Add get todo by id

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
+const {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -28,6 +29,30 @@ app.get("/todos", (req, res)=> {
     res.status(400).send(err);
   })
 })
+
+app.get("/todos", (req, res)=> {
+  Todo.find().then((todos)=>{
+    res.send({todos});
+  },  (err)  =>{
+    res.status(400).send(err);
+  })
+})
+
+app.get("/todos/:id", (req, res)=> {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send("Id is invalid");
+  }
+  Todo.findById(id).then((todo)=>{
+    if (!todo) {
+      return res.status(404).send("user not found");
+    }
+    res.send({todo});
+  }).catch((e)=> {
+    console.log(e);
+  })
+});
+
 app.listen(3000, ()=> {
   console.log("Server started at port 3000");
 })
